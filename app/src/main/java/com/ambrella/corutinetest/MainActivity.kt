d6ab0a7c947c56ce21f2644b66c1e59746c0c0da
Prilepskiy_AE <woolf7754286@yandex.ru>
2022-02-03 23:12:03 +0700

    test Handler

--- app/src/main/java/com/ambrella/corutinetest/MainActivity.kt
+++ app/src/main/java/com/ambrella/corutinetest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ambrella.corutinetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.ambrella.corutinetest.databinding.ActivityMainBinding
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             loadData()
         }
     }
-//private val handler:Handler=
+//private val handler:Handler= Handler()
     private fun loadData() {
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
@@ -38,19 +39,30 @@ class MainActivity : AppCompatActivity() {
     private fun loadCity(callback: (String) -> Unit) {
         thread {
             Thread.sleep(5000)
-            callback.invoke("Moscow")
+            Handler(Looper.getMainLooper()).post {
+                callback.invoke("Moscow")
+            }
+
         }
     }
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
-        thread {
-            Toast.makeText(
-                this,
-                getString(R.string.loading_temperature_toast, city),
-                Toast.LENGTH_SHORT
-            ).show()
+
+        //Handler(Looper.getMainLooper()).post <===>  runOnUiThread
             Thread.sleep(5000)
-            callback.invoke(17)
+            thread {
+                runOnUiThread {
+
+                    Toast.makeText(
+                            this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+                Handler(Looper.getMainLooper()).post {
+                    callback.invoke(17)
+                }
         }
     }
 }
